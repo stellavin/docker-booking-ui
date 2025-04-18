67e2dccba0a63b6337193e721fc2c46d8acc5252
Stella Sikhila <stellavinmaris@gmail.com>
2025-04-18 19:06:44 +0300

    fixed navbar text alighnment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Navbar = () => (
   <nav className="bg-navbar text-white px-4 py-3 shadow-md">
-    <div className="max-w-6xl mx-auto flex items-center">
-      <h1 className="text-2xl font-bold">Doctor Directory</h1>
+    <div className="max-w-7xl mx-auto flex items-center">
+      <h1 className="text-2xl font-bold">Doctor's Directory</h1>
     </div>
   </nav>
 );
